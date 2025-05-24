4d4ec180c2a294429b09cdd93f4d29d7a6e8a6fc
D Yermachenko <d.yermachenko@gmail.com>
2025-05-25 01:05:39 +0200

    Initial commit

---
+++ MyMediator.Tests/Abstractions/MockImplementation/CalculationRequestResponce.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using MyMediator.Abstraction;
+
+namespace MyMediator.Tests.Abstractions.MockImplementation;
+
+public interface ITwoNumbersRequest
+{
+    int NumberOne { get; init; }
+
+    ITwoNumbersRequest AlterNumberOne(Func<int, int> alter);
+
+    int NumberTwo { get; init; }
+
+    ITwoNumbersRequest AlterNumberTwo(Func<int, int> alter);
+}
+
+public interface ITwoNumberResult
+{
+    double Result{ get; }
+
+    ITwoNumberResult AlterResult(Func<double, double> alter);
+}
+
+#region Sum Query
+
+public record SumRequest(int NumberOne, int NumberTwo) : IRequest<SumResponse>, ITwoNumbersRequest
+{
+    public ITwoNumbersRequest AlterNumberOne(Func<int, int> alter) => this with { NumberOne = alter?.Invoke(NumberOne) ?? NumberOne };
+
+    public ITwoNumbersRequest AlterNumberTwo(Func<int, int> alter) => this with { NumberTwo = alter?.Invoke(NumberTwo) ?? NumberTwo };
+}
+
+public record SumResponse(double Result) : ITwoNumberResult
+{
+    public ITwoNumberResult AlterResult(Func<double, double> alter) => this with { Result = alter?.Invoke(Result) ?? Result };
+
+}
+
+public class SumRequestHandler : IRequestHandler<SumRequest, SumResponse>
+{
+    public Task<SumResponse> HandleAsync(SumRequest request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(new SumResponse(request.NumberOne + request.NumberTwo) );
+    }
+}
+#endregion
+
+#region Difference Query
+public record DifRequest(int NumberOne, int NumberTwo) : IRequest<DifResponse>, ITwoNumbersRequest
+{
+    public ITwoNumbersRequest AlterNumberOne(Func<int, int> alter) => this with { NumberOne = alter?.Invoke(NumberOne) ?? NumberOne };
+
+    public ITwoNumbersRequest AlterNumberTwo(Func<int, int> alter) => this with { NumberTwo = alter?.Invoke(NumberTwo) ?? NumberTwo };
+}
+
+public record DifResponse(double Result) : ITwoNumberResult
+{
+    public ITwoNumberResult AlterResult(Func<double, double> alter) => this with { Result = alter?.Invoke(Result) ?? Result };
+}
+
+public class DifRequestHandler : IRequestHandler<DifRequest, DifResponse>
+{
+    public Task<DifResponse> HandleAsync(DifRequest request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(new DifResponse(request.NumberOne + request.NumberTwo));
+    }
+}
+
+#endregion
+
+#region Another Sum Query
+
+public record AnotherSumRequest(int NumberOne, int NumberTwo) : IRequest<AnotherSumResponse>;
+
+public record AnotherSumResponse(double Result) ;
+
+
+public class AnotherSumRequestHandler : IRequestHandler<AnotherSumRequest, AnotherSumResponse>
+{
+    public Task<AnotherSumResponse> HandleAsync(AnotherSumRequest request, CancellationToken cancellationToken = default)
+    {
+        return Task.FromResult(new AnotherSumResponse(request.NumberOne + request.NumberTwo));
+    }
+}
+#endregion
+
+#region Two number behaviours
+
+public class MultiplyArgsInTwoBehaviour<TRequest, TResponse> : IRequestBehaviour<TRequest, TResponse>
+    where TRequest : IRequest<TResponse> 
+{
+    public Task<TResponse> HandleAsync(TRequest request, RequestHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken = default)
+    {
+        if(request is ITwoNumbersRequest r)
+            request = (TRequest)r.AlterNumberOne(x => x * 2).AlterNumberTwo(x => x * 2);
+        return next(request, cancellationToken);
+    }
+}
+
+public class MultiplyAnswerInTwoBehaviour<TRequest, TResponse> : IRequestBehaviour<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+
+{
+    public async Task<TResponse> HandleAsync(TRequest request, RequestHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken = default)
+    {
+        var result = await next(request) ;
+        if(result is ITwoNumberResult tnr)
+            result = (TResponse)tnr.AlterResult(x => x * 2);
+        return result;
+    }
+}
+
+public class MultiplyResultAndArgsInTwoBehaviour<TRequest, TResponse> : IRequestBehaviour<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> HandleAsync(TRequest request, RequestHandlerDelegate<TRequest, TResponse> next, CancellationToken cancellationToken = default)
+    {
+        if (request is ITwoNumbersRequest r)
+            request = (TRequest)r.AlterNumberOne(x => x * 2).AlterNumberTwo(x => x * 2);
+        var result = await next(request);
+        if (result is ITwoNumberResult tnr)
+            result = (TResponse)tnr.AlterResult(x => x * 2);
+
+        return result;
+    }
+}
+
+#endregion
+
+#region Two number Decorators
+
+public class MultiplyAnswerByTwoDecorator<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> Next) : IRequestDecorator<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        var result = await Next.HandleAsync(request);
+        if (result is ITwoNumberResult tnr)
+            result = (TResponse)tnr.AlterResult(x => x * 2);
+        return result;
+    }
+}
+
+public class MultiplyArgsInTwoDecorator<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> Next) : IRequestDecorator<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request is ITwoNumbersRequest r)
+            request = (TRequest)r.AlterNumberOne(x => x * 2).AlterNumberTwo(x => x * 2);
+        return await Next.HandleAsync(request, cancellationToken);
+    }
+}
+
+public class MultiplyArgumentsAndResultByTwoDecorator<TRequest, TResponse>(IRequestHandler<TRequest, TResponse> Next) : IRequestDecorator<TRequest, TResponse>
+    where TRequest : IRequest<TResponse>
+{
+    public async Task<TResponse> HandleAsync(TRequest request, CancellationToken cancellationToken = default)
+    {
+        if (request is ITwoNumbersRequest r)
+            request = (TRequest)r.AlterNumberOne(x => x * 2).AlterNumberTwo(x => x * 2);
+        var result = await Next.HandleAsync(request);
+        if (result is ITwoNumberResult tnr)
+            result = (TResponse)tnr.AlterResult(x => x * 2);
+
+        return result;
+    }
+}
+
+#endregion
---
+++ MyMediator.Tests/Tests/TestAddCommandStyleRequests.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using MyMediator.Abstraction;
+using MyMediator.Tests.Abstractions.MockImplementation;
+using MyMediator.Mediator.DIExtension;
+using MyMediator.Decorators.DIExtension;
+
+namespace MyMediator.Tests.Tests;
+
+public class TestAddCommandStyleRequests
+{
+
+    [Fact]
+    public async Task Command_Executes_Solo()
+    {
+        ServiceCollection services = new();
+        services
+            .AddMyMediator()
+            .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
+            .AddSingleton<ISomeExternalService, SomeExternalService>()
+            ;
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
+        FooWritingCommand fooWritingCommand = new("Here we writing foo");
+        ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
+
+        await myMediator.SendAsync(fooWritingCommand);
+
+        Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
+        Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
+    }
+
+    [Fact]
+    public async Task Command_Executes_With_Decorator()
+    {
+        ServiceCollection services = new();
+        services
+            .AddMyMediator()
+            .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
+            .AddSingleton<ISomeExternalService, SomeExternalService>()
+            .AddDecorator(typeof(StorageServiceDecorator<>))
+            //.AddDecorator(typeof(MultiplyAnswerByTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgsInTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgumentsAndResultByTwoDecorator<,>))
+            //.AddBehaviour(typeof(MultiplyArgsInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyAnswerInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyResultAndArgsInTwoBehaviour<,>))
+            ;
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
+        FooWritingCommand fooWritingCommand = new("Here we writing foo");
+        ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
+
+        await myMediator.SendAsync(fooWritingCommand);
+
+        Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
+        Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
+        Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Decorator"), "DECORATOR");
+    }
+
+    [Fact]
+    public async Task Command_Executes_With_Behaviour()
+    {
+        ServiceCollection services = new();
+        services
+            .AddMyMediator()
+            .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
+            .AddSingleton<ISomeExternalService, SomeExternalService>()
+            .AddBehaviour(typeof(StorageServiceBehaviour<>))
+            
+            //.AddDecorator(typeof(MultiplyAnswerByTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgsInTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgumentsAndResultByTwoDecorator<,>))
+            //.AddBehaviour(typeof(MultiplyArgsInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyAnswerInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyResultAndArgsInTwoBehaviour<,>))
+            ;
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
+        FooWritingCommand fooWritingCommand = new("Here we writing foo");
+        ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
+
+        await myMediator.SendAsync(fooWritingCommand);
+
+        Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
+        Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
+        Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Behaviour"), "BEHAVIOUR");
+    }
+
+    [Fact]
+    public async Task Command_Executes_With_Behaviour_And_Decorator()
+    {
+        ServiceCollection services = new();
+        services
+            .AddMyMediator()
+            .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
+            .AddSingleton<ISomeExternalService, SomeExternalService>()
+            .AddBehaviour(typeof(StorageServiceBehaviour<>))
+            .AddDecorator(typeof(StorageServiceDecorator<>))
+            //.AddDecorator(typeof(MultiplyAnswerByTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgsInTwoDecorator<,>))
+            //.AddDecorator(typeof(MultiplyArgumentsAndResultByTwoDecorator<,>))
+            //.AddBehaviour(typeof(MultiplyArgsInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyAnswerInTwoBehaviour<,>))
+            //.AddBehaviour(typeof(MultiplyResultAndArgsInTwoBehaviour<,>))
+            ;
+        IServiceProvider serviceProvider = services.BuildServiceProvider();
+        var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
+        FooWritingCommand fooWritingCommand = new("Here we writing foo");
+        ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
+
+        await myMediator.SendAsync(fooWritingCommand);
+
+        Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
+        Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
+        Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Behaviour"), "BEHAVIOUR");
+        Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Decorator"), "DECORATOR");
+    }
+
+
+}
