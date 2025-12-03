9425b98e64fb316e212ad4c6967fc169906e8605
D Yermachenko <d.yermachenko@gmail.com>
2025-12-03 11:30:15 +0100

    Minor change in test file

--- DyoMediator.Tests/Tests/TestAddCommandStyleRequests.cs
+++ DyoMediator.Tests/Tests/TestAddCommandStyleRequests.cs
@@ -12,6 +12,7 @@ public class TestAddCommandStyleRequests
     [Fact]
     public async Task Command_Executes_Solo()
     {
+        // Arrange
         ServiceCollection services = new();
         services
             .AddMyMediator()
@@ -22,9 +23,9 @@ public class TestAddCommandStyleRequests
         var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
         FooWritingCommand fooWritingCommand = new("Here we writing foo");
         ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
-
+        // Act
         await myMediator.SendAsync(fooWritingCommand);
-
+        // Assert
         Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
         Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
     }
@@ -60,27 +61,23 @@ public class TestAddCommandStyleRequests
     [Fact]
     public async Task Command_Executes_With_Behaviour()
     {
+        // Arrange
         ServiceCollection services = new();
         services
             .AddMyMediator()
             .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
             .AddSingleton<ISomeExternalService, SomeExternalService>()
-            .AddBehaviour(typeof(StorageServiceBehaviour<>))
-            
-            //.AddDecorator(typeof(MultiplyAnswerByTwoDecorator<,>))
-            //.AddDecorator(typeof(MultiplyArgsInTwoDecorator<,>))
-            //.AddDecorator(typeof(MultiplyArgumentsAndResultByTwoDecorator<,>))
-            //.AddBehaviour(typeof(MultiplyArgsInTwoBehaviour<,>))
-            //.AddBehaviour(typeof(MultiplyAnswerInTwoBehaviour<,>))
-            //.AddBehaviour(typeof(MultiplyResultAndArgsInTwoBehaviour<,>))
-            ;
+            .AddBehaviour(typeof(StorageServiceBehaviour<>));
+
         IServiceProvider serviceProvider = services.BuildServiceProvider();
         var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
         FooWritingCommand fooWritingCommand = new("Here we writing foo");
         ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
 
+        // Act
         await myMediator.SendAsync(fooWritingCommand);
 
+        // Assert
         Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
         Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
         Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Behaviour"), "BEHAVIOUR");
@@ -89,27 +86,23 @@ public class TestAddCommandStyleRequests
     [Fact]
     public async Task Command_Executes_With_Behaviour_And_Decorator()
     {
+        // Arrange
         ServiceCollection services = new();
         services
             .AddMyMediator()
             .AddHandler<FooWritingCommand, FooWritingCommandHandler>()
             .AddSingleton<ISomeExternalService, SomeExternalService>()
             .AddBehaviour(typeof(StorageServiceBehaviour<>))
-            .AddDecorator(typeof(StorageServiceDecorator<>))
-            //.AddDecorator(typeof(MultiplyAnswerByTwoDecorator<,>))
-            //.AddDecorator(typeof(MultiplyArgsInTwoDecorator<,>))
-            //.AddDecorator(typeof(MultiplyArgumentsAndResultByTwoDecorator<,>))
-            //.AddBehaviour(typeof(MultiplyArgsInTwoBehaviour<,>))
-            //.AddBehaviour(typeof(MultiplyAnswerInTwoBehaviour<,>))
-            //.AddBehaviour(typeof(MultiplyResultAndArgsInTwoBehaviour<,>))
-            ;
+            .AddDecorator(typeof(StorageServiceDecorator<>));
         IServiceProvider serviceProvider = services.BuildServiceProvider();
         var myMediator = serviceProvider.GetRequiredService<IMyMediator>();
         FooWritingCommand fooWritingCommand = new("Here we writing foo");
         ISomeExternalService someExternalService = serviceProvider.GetRequiredService<ISomeExternalService>();
 
+        // Act
         await myMediator.SendAsync(fooWritingCommand);
-
+        
+        // Assert
         Assert.NotEmpty(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"));
         Assert.Equal(someExternalService.GetValue(fooWritingCommand.Key + "-Handler"), fooWritingCommand.Value);
         Assert.Contains(someExternalService.GetValue(fooWritingCommand.Key + "-Behaviour"), "BEHAVIOUR");
